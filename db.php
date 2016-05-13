82dbcd5db4c014723358be14afcf7b32a1ff6b2b
edgar mendoza <eeduardo>
2016-05-12 22:28:45 -0600

    app

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$con=mysqli_connect("localhost","root","root","phonegap_demo");
-$conexion = new mysqli("localhost","root","root","phonegap_demo");
+$con=mysqli_connect("52.37.73.110","root","root","phonegap_demo");
+$conexion = new mysqli("52.37.73.110","root","root","phonegap_demo");
 ?>
